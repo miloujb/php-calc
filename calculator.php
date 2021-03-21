5082b9de51c74b4f059e7027cf627c23dcfd6f28
miloujb <emilybailey@Emilys-MacBook-Air.local>
2021-03-21 19:31:30 +0000

    basic completion

--- calculator.php
+++ calculator.php
@@ -18,5 +18,30 @@
     <br />
     <button type="submit" name="submit" value="submit">Calculate</button>
 </form>
+<p>The answer is:</p>
+<?php
+    if (isset($_GET['submit'])) {
+        $result1 = $_GET['num1'];
+        $result2 = $_GET['num2'];
+        $operator = $_GET['operator'];
+        switch ($operator) {
+            case None:
+                echo "Error: Please select a method";
+            break;
+            case Add: 
+                echo $result1 + $result2;
+            break;
+            case Subtract:
+                echo $result1 - $result2;
+                break;
+            case Multiply:
+                echo $result1 * $result2;
+                break;
+            case Divide: 
+                echo $result1 / $result2;
+                break;
+        }
+    }
+?>
 </body>
 </html>
