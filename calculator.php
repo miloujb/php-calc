e81f29747f5dd2391fde76cdfd9b1cb92148ac83
miloujb <emilybailey@Emilys-MacBook-Air.local>
2021-03-21 19:20:02 +0000

    initial

---
+++ calculator.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Calculator</title>
+</head>
+<body>
+<form>
+    <input type="text" name="num1" placeholder="Number 1">
+    <input type="text" name="num2" placeholder="Number 2">
+    <select name="operator">
+    <option>None</option>
+    <option>Add</option>
+    <option>Subtract</option>
+    <option>Multiply</option>
+    <option>Divide</option>
+    </select>
+    <br />
+    <button type="submit" name="submit" value="submit">Calculate</button>
+</form>
+</body>
+</html>
